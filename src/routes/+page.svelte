d640c4dfc53bbafe214d266dd2dd532640531a1d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-13 00:25:10 -0500

    feat: change the architecture of routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,45 @@
 <script lang="ts">
-  import wordlist from '$lib/data/words';
-  import Card from '$lib/components/Card.svelte';
+  import { goto } from '$app/navigation';
+  const word_options_num: number[] = [5, 10, 15, 20];
+  const levels: string[] = ['kindergarten', 'first_grade'];
+  let level_chosen: boolean;
+  let question_num_chosen: boolean;
+  let level: string;
+  let question_num: number;
 
-  let index = 0;
-
-  $: redword = wordlist[index % wordlist.length];
-
-  function next() {
-    index = ++index;
+  // Function to run which sets the lelvel_chosen to true and captures the value of the level on button click
+  function handle_levels(event: Event) {
+    level_chosen = true;
+    level = (event.target as HTMLButtonElement).innerText;
   }
 
-  function previous() {
-    if (index > 0) {
-      index = --index;
-    }
+  // Function to run which sets the question_num_chosen to true and captures the value of the number of questions on button click
+  function handle_question_num(event: Event) {
+    question_num_chosen = true;
+    question_num = parseInt((event.target as HTMLButtonElement).innerText);
   }
 </script>
 
-<div id="cardwindow">
-  <button type="button" on:click|preventDefault={previous}>Previous</button>
-  <Card {redword}></Card>
-  <button type="button" on:click|preventDefault={next}>Next</button>
-</div>
+<h1>Welcome to Red Word Practice</h1>
 
-<style>
-  #cardwindow {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    padding: 2rem;
-  }
-  button {
-    width: 30%;
-    height: 100%;
-  }
-</style>
+{#if !level_chosen}
+  <h2>Choose a level</h2>
+  {#each levels as level}
+    <button on:click={handle_levels}>{level}</button>
+  {/each}
+{/if}
+
+{#if level_chosen && !question_num_chosen}
+  <h2>Choose the number of questions</h2>
+  <h3>Level: {level}</h3>
+  {#each word_options_num as num}
+    <button on:click={handle_question_num}>{num}</button>
+  {/each}
+{/if}
+
+{#if level_chosen && question_num_chosen}
+  <h2>Start the test</h2>
+  <h3>Level: {level}</h3>
+  <h3>Number of words: {question_num}</h3>
+  <button on:click={() => goto(`/${level}/${question_num}`)}>Start</button>
+{/if}
---
+++ src/routes/[level]/[number]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { PageData, PageServerData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<h1>Welcome to Red Word Practice</h1>
+
+<p>Your selected level is: {data.level}</p>
+<p>Your selected number of words is: {data.number}</p>
