f39396aa2ee4ef98d47e97d306a49b887d7040eb
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-13 09:36:44 -0500

    docs: add documentation for JSDoc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,51 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  const word_options_num: number[] = [5, 10, 15, 20];
+  const wordNumOptions: number[] = [5, 10, 15, 20];
   const levels: string[] = ['kindergarten', 'first_grade'];
-  let level_chosen: boolean;
-  let question_num_chosen: boolean;
+  let levelChosen: boolean;
+  let questionNumChosen: boolean;
   let level: string;
-  let question_num: number;
+  let questionNum: number;
 
-  // Function to run which sets the lelvel_chosen to true and captures the value of the level on button click
-  function handle_levels(event: Event) {
-    level_chosen = true;
+  /**
+   * Handles the level selection event.
+   * @param event - The event triggered by clicking a level button.
+   */
+  function handleLevel(event: Event) {
+    levelChosen = true;
     level = (event.target as HTMLButtonElement).innerText;
   }
 
-  // Function to run which sets the question_num_chosen to true and captures the value of the number of questions on button click
-  function handle_question_num(event: Event) {
-    question_num_chosen = true;
-    question_num = parseInt((event.target as HTMLButtonElement).innerText);
+  /**
+   * Handles the question number selection event.
+   * @param event - The event triggered by clicking a question button.
+   */
+  function handleQuestionNum(event: Event): void {
+    questionNumChosen = true;
+    questionNum = parseInt((event.target as HTMLButtonElement).innerText);
   }
 </script>
 
 <h1>Welcome to Red Word Practice</h1>
 
-{#if !level_chosen}
+{#if !levelChosen}
   <h2>Choose a level</h2>
   {#each levels as level}
-    <button on:click={handle_levels}>{level}</button>
+    <button on:click={handleLevel}>{level}</button>
   {/each}
 {/if}
 
-{#if level_chosen && !question_num_chosen}
+{#if levelChosen && !questionNumChosen}
   <h2>Choose the number of questions</h2>
   <h3>Level: {level}</h3>
-  {#each word_options_num as num}
-    <button on:click={handle_question_num}>{num}</button>
+  {#each wordNumOptions as num}
+    <button on:click={handleQuestionNum}>{num}</button>
   {/each}
 {/if}
 
-{#if level_chosen && question_num_chosen}
+{#if levelChosen && questionNumChosen}
   <h2>Start the test</h2>
   <h3>Level: {level}</h3>
-  <h3>Number of words: {question_num}</h3>
-  <button on:click={() => goto(`/${level}/${question_num}`)}>Start</button>
+  <h3>Number of words: {questionNum}</h3>
+  <button on:click={() => goto(`/${level}/${questionNum}`)}>Start</button>
 {/if}
