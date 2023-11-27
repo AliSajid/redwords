84445b675a45718b5ba7da168397c18f2b366b3f
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-27 11:50:13 -0500

    feat: add the basic structure of the admin interface

---
+++ src/routes/(admin)/+layout.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import Footer from '$lib/components/Footer.svelte';
+  import Header from '$lib/components/Header.svelte';
+</script>
+
+<Header />
+<div class="container-fluid">
+  <aside>
+    <h3>Admin</h3>
+    <nav>
+      <ul>
+        <li><a href="/admin">Home</a></li>
+        <li><a href="/admin/current">Current Words</a></li>
+        <li><a href="/admin/add_new">Add Words</a></li>
+      </ul>
+    </nav>
+  </aside>
+  <main>
+    <slot />
+  </main>
+</div>
+<Footer />
+
+<style>
+  .container-fluid {
+    display: flex;
+    height: 100%;
+  }
+  aside {
+    width: 20%;
+    height: 100%;
+    background-color: #f0f0f0;
+    padding: 1rem;
+  }
+
+  main {
+    width: 80%;
+    height: 100%;
+    padding: 1rem;
+  }
+
+  li {
+    padding: 1rem;
+    border-bottom: 1px solid #ccc;
+  }
+
+  li:first-child {
+    border-top: 1px solid #ccc;
+  }
+</style>
---
+++ src/routes/(admin)/admin/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<sveltekit:head>
+  <title>Admin Interface</title>
+</sveltekit:head>
+
+<h1>Admin Interface</h1>
+
+<p>Admin interface for managing the word list.</p>
