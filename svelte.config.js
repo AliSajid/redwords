7735c55c33221cffad0e28f86e3ff7874a47e5dd
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-27 14:13:44 -0500

    build: moved the unplugin config to vite config

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import Icons from 'unplugin-icons/vite';
 
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  plugins: [
-    // eslint-disable-next-line new-cap
-    Icons({
-      compiler: 'svelte',
-    }),
-  ],
+  plugins: [],
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit(),
+    // eslint-disable-next-line new-cap
+    Icons({
+      compiler: 'svelte',
+    }),
+  ],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}'],
   },
