340b384124d8fcc623653f68cdddf43f0e16df4b
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-27 11:55:56 -0500

    feat: add information on the admin homepage on how to get things to work

--- src/routes/(admin)/admin/+page.svelte
+++ src/routes/(admin)/admin/+page.svelte
@@ -10,4 +10,21 @@
 
 <h1>Admin Interface</h1>
 
-<p>Admin interface for managing the word list.</p>
+<p>This is the admin interface allowing the addition and removal of words from the database.</p>
+<p>We can also use this interface to generate audio recordings for the words.</p>
+
+<p>You can use the navigation menu on the left to navigate to the different pages of the admin interface.</p>
+<ul>
+  <li>
+    The
+    <strong>Home</strong> page is the page you are currently on.
+  </li>
+  <li>
+    The <strong>Current Words</strong> page allows you to view the words that are currently in the database. You can edit
+    and remove words from this page.
+  </li>
+  <li>
+    The <strong>Add Words</strong> page allows you to add new words to the database. You can either add words one at a time
+    or you can upload a CSV file containing the words and their reading level.
+  </li>
+</ul>
