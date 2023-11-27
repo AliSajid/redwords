9f45334bb6a381b6d10837d2200208c33561f371
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-27 10:00:09 -0500

    feat: make the main page more informative and with a form
    fixes #27 #29 #30 #28

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,52 +1,50 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
-  import wordLevelMapping from '$lib/utils/WordLevelMapping';
 
-  const wordNumOptions: number[] = [5, 10];
-  const levels: string[] = ['kindergarten', 'first_grade'];
-  let levelChosen: boolean;
-  let questionNumChosen: boolean;
   let level: string;
   let questionNum: number;
-
-  /**
-   * Handles the level selection event.
-   * @param event - The event triggered by clicking a level button.
-   */
-  function handleLevel(event: Event) {
-    levelChosen = true;
-    const levelText = (event.target as HTMLButtonElement).innerText;
-    level = wordLevelMapping[levelText as keyof typeof wordLevelMapping];
-  }
-
-  /**
-   * Handles the question number selection event.
-   * @param event - The event triggered by clicking a question button.
-   */
-  function handleQuestionNum(event: Event): void {
-    questionNumChosen = true;
-    questionNum = parseInt((event.target as HTMLButtonElement).innerText);
-  }
 </script>
 
-{#if !levelChosen}
-  <h2>Choose a level</h2>
-  {#each levels as level}
-    <button on:click={handleLevel}>{level}</button>
-  {/each}
-{/if}
+<div id="questionsMain">
+  <p>
+    Welcome to this small app that helps you learn to read words by sight. You can use the form below to select the
+    level and the number of words you need to practice. Once you are ready, click the start button to begin the practice
+    session.
+  </p>
 
-{#if levelChosen && !questionNumChosen}
-  <h2>Choose the number of questions</h2>
-  <h3>Level: {level}</h3>
-  {#each wordNumOptions as num}
-    <button on:click={handleQuestionNum}>{num}</button>
-  {/each}
-{/if}
+  <form>
+    <label for="level">Level</label>
+    <select id="level" name="level" bind:value={level}>
+      <option value="KG">Kindergarten</option>
+      <option value="GR1">First Grade</option>
+    </select>
+    <label for="number">Number of words</label>
+    <select id="numberQuestions" name="numberQuestions" bind:value={questionNum}>
+      <option value="5">5</option>
+      <option value="10">10</option>
+    </select>
+    <button type="submit" on:click|preventDefault={() => goto(`/${level}/${questionNum}`)}>Start</button>
+  </form>
+</div>
 
-{#if levelChosen && questionNumChosen}
-  <h2>Start the test</h2>
-  <h3>Level: {level}</h3>
-  <h3>Number of words: {questionNum}</h3>
-  <button on:click={() => goto(`/${level}/${questionNum}`)}>Start</button>
-{/if}
+<style>
+  form {
+    align-items: center;
+    width: 50%;
+  }
+
+  form > * {
+    margin: 1rem;
+  }
+
+  p {
+    margin: 1rem;
+    width: 50%;
+  }
+
+  #questionsMain {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
