c8ea2e97508848992f117d1442493a358d1d19d2
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-27 14:47:42 -0500

    feat: add a current_words table

--- src/routes/(admin)/admin/current/+page.server.ts
+++ src/routes/(admin)/admin/current/+page.server.ts
@@ -1,5 +1,23 @@
 import type { PageServerLoad } from './$types';
+import prisma from '$lib/utils/PrismaClient';
 
 export const load = (async () => {
-  return {};
+  const wordlist = await prisma.redWord
+    .findMany({
+      select: { id: true, word: true, level: { select: { level_name: true, level_display_name: true } } },
+      orderBy: { createdAt: 'asc' },
+    })
+    .then((words) => {
+      return words.map((word) => {
+        return {
+          id: word.id,
+          word: word.word,
+          level: word.level.level_name,
+          level_display_name: word.level.level_display_name,
+          audio_available: false,
+        };
+      });
+    });
+
+  return { words: wordlist };
 }) satisfies PageServerLoad;
