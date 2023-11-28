d3b1c6b1d0ac59b0bc75123dd707c1a274de687a
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-28 12:27:47 -0500

    feat: add components for edmin buttons

---
+++ src/lib/components/AdminButtonPanel.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type PrismaRedWordResult from '$lib/types/PrismaRedWordResult';
+  import AdminButton from './AdminButton.svelte';
+  export let row: PrismaRedWordResult;
+
+  let type;
+</script>
+
+<AdminButton type="edit" {row} />
+<AdminButton type="delete" {row} />
