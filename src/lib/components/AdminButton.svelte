99ce87d025176b6fab7f8d990ff3e00ac9014fae
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-28 12:27:47 -0500

    feat: add components for edmin buttons

---
+++ src/lib/components/AdminButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type PrismaRedWordResult from '$lib/types/PrismaRedWordResult';
+
+  export let row: PrismaRedWordResult;
+  export let type: 'edit' | 'delete';
+
+  function generateHref() {
+    if (type === 'edit') {
+      return `/admin/words/${row.id}/edit`;
+    } else if (type === 'delete') {
+      return `/admin/words/${row.id}/delete`;
+    }
+  }
+
+  const href = generateHref();
+  const label = type === 'edit' ? 'Edit' : 'Delete';
+</script>
+
+<a {href} role="button">{label}</a>
