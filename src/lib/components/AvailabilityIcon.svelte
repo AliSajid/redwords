c9ce38ab2fa2dfe3846f4f648f396a6dd50fe0de
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-28 12:28:57 -0500

    feat: add an availability icon as a component

---
+++ src/lib/components/AvailabilityIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type PrismaRedWordResult from '$lib/types/PrismaRedWordResult';
+  import IconCheckOutline from '~icons/mdi/check-outline';
+  import IconCrossMark from '~icons/openmoji/cross-mark';
+
+  export let row: PrismaRedWordResult;
+</script>
+
+{#if row.audioAvailable}
+  <IconCheckOutline style="font-size: 3em; color: green" />
+{:else}
+  <IconCrossMark style="font-size: 4em" />
+{/if}
