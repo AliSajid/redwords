60ebcf6ebb118c4dfa665c377b4b0115e8428260
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-29 01:08:36 -0500

    feat: add functioning form for adding new words

---
+++ src/routes/(admin)/admin/words/new/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { enhance } from '$app/forms';
+
+  let creating = false;
+  let value = '';
+</script>
+
+<h1>Add New Word</h1>
+
+<form
+  method="POST"
+  use:enhance={({ formElement, formData, action, cancel, submitter }) => {
+    creating = true;
+
+    return async ({ result, update }) => {
+      creating = false;
+      await update();
+    };
+  }}
+>
+  <label for="word">Word</label>
+  <input type="text" name="word" id="word" required />
+
+  <label for="level">Word Level</label>
+  <select name="wordLevel" id="wordLevel" bind:value>
+    <option value="KG">Kindergarten</option>
+    <option value="GR1">First Grade</option>
+    <option value="GR2">Second Grade</option>
+    <option value="GR3">Third Grade</option>
+    <option value="HIGH">Higher Grades</option>
+  </select>
+
+  <button type="submit" disabled={creating} aria-busy={creating} class:secondary={creating}>Add Word</button>
+</form>
