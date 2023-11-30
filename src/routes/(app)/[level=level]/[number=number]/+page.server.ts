3636de613de579f6942166aa644fec06a1a0d9d0
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-30 12:44:40 -0500

    feat: return random sublists of words

--- src/routes/(app)/[level=level]/[number=number]/+page.server.ts
+++ src/routes/(app)/[level=level]/[number=number]/+page.server.ts
@@ -4,15 +4,36 @@ import prisma from '$lib/utils/PrismaClient';
 export const load: PageServerLoad = async ({ params }) => {
   const level: string = params.level;
 
-  const numWords = parseInt(params.number, 10);
+  const numWords = parseInt(params.number) || 5;
+
+  console.log(numWords);
 
   const wordLevel = await prisma.wordLevel.findUnique({
     where: { levelName: level },
   });
 
+  const wordIds = await prisma.redWord
+    .findMany({
+      where: { level: { levelName: level } },
+      select: { id: true },
+    })
+    .then((words) => {
+      return words
+        .map((word) => word.id)
+        .map((value) => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
+    });
+
+  console.log(wordIds);
+
+  const selectedWords = wordIds.slice(0, numWords);
+
+  console.log(selectedWords);
+
   const wordlist = await prisma.redWord
     .findMany({
-      where: { levelId: wordLevel?.id },
+      where: { levelId: wordLevel?.id, id: { in: selectedWords } },
       select: { id: true, level: true, word: true },
       take: numWords,
     })
@@ -27,6 +48,8 @@ export const load: PageServerLoad = async ({ params }) => {
         }),
       );
     });
+
+  console.log(wordlist);
 
   return {
     props: {
